b3e985d65d5086d59a37998f0b1b84237fc6ba7d
anithaah <anithaaha196@gmail.com>
2026-02-20 15:25:56 +0530

    5th commit

--- src/test/java/GithubPractice/AmazonMarati.java
+++ src/test/java/GithubPractice/AmazonMarati.java
@@ -3,5 +3,6 @@ package GithubPractice;
 public class AmazonMarati {
 public static void main(String[] args) {
 	System.out.println("a1");
+	System.out.println("a2");
 }
 }
--- src/test/java/GithubPractice/TestAutomationPractice_Pagination.java
+++ src/test/java/GithubPractice/TestAutomationPractice_Pagination.java
@@ -3,6 +3,7 @@ package GithubPractice;
 public class TestAutomationPractice_Pagination {
 	public static void main(String[] args) {
 		System.out.println("helo");
+		System.out.println("hello-2");
 	}
 }
 	
