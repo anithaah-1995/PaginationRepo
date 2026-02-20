55ffa9a1d5cf024631efeb88c5f4a3cb62282483
anithaah <anithaaha196@gmail.com>
2026-02-20 16:26:04 +0530

    added 4 sops

--- src/test/java/GithubPractice/TestAutomationPractice_Pagination.java
+++ src/test/java/GithubPractice/TestAutomationPractice_Pagination.java
@@ -2,6 +2,10 @@ package GithubPractice;
 
 public class TestAutomationPractice_Pagination {
 	public static void main(String[] args) {
+		
+		System.out.println("helo");
+		System.out.println("helo");
+		System.out.println("helo");
 		System.out.println("helo");
 	}
 }
